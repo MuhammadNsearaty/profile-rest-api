012752812f2b07527751d35c04aa44fa7f7bb1ef
MuhammadNsearaty <mnsyrat057@gmail.com>
2021-04-02 09:43:42 +0300

    add user profile model to admin models

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -25,7 +25,7 @@ class UserProfileManager(BaseUserManager):
         user = self.create_user(email, name, password)
 
         user.is_superuser = True
-        user.is_staff = TrueS
+        user.is_staff = True
         user.save(using=self._db)
 
         return user
