9e9fcb431c31c9190f815648485ef4ec601e25a0
MuhammadNsearaty <mnsyrat057@gmail.com>
2021-04-09 16:55:34 +0300

    added search

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,10 +3,11 @@ from rest_framework.response import Response
 from rest_framework import viewsets
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
-
+from rest_framework import filters
 from profiles_api import models
 from profiles_api import serializers
 from profiles_api import permissions
+
 
 class UserProfileViewSet(viewsets.ModelViewSet):
     """Handle creating and updating profiles"""
@@ -16,6 +17,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name','email')
 
 class HelloApiView(APIView):
     """Test API View"""
