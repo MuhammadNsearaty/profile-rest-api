13d041a50420b41303e43a2b7e0728a84b9006b0
MuhammadNsearaty <mnsyrat057@gmail.com>
2021-04-09 16:48:02 +0300

    added permissions

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -5,7 +5,7 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, basename='hello-viewset')
-
+router.register('profile',views.UserProfileViewSet)
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
     path('', include(router.urls)),
