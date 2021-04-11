77c231fd51d9081328232a2acd0bb7adfe275712
MuhammadNsearaty <mnsyrat057@gmail.com>
2021-04-11 14:58:47 +0300

    added profile feed Api with permissions

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -1,6 +1,18 @@
 from rest_framework import serializers
 from profiles_api import models
 
+class ProfileFeedItemSerializer(serializers.ModelSerializer):
+    """Serializes profile fees item"""
+
+    class Meta:
+        model = models.ProfileFeedItem
+        fields = ('id', 'user_profile','status_text', 'created_on')
+        extra_kwargs = {
+            'user_profile':{
+                'read_only':True,
+                }
+        }
+
 class HelloSerializer(serializers.Serializer):
     """Serializes a name field for testing out APIView"""
     name = serializers.CharField(max_length=10)
