4d9d22861dce120c55cd50f224b2cc411d9b6466
MuhammadNsearaty <mnsyrat057@gmail.com>
2021-05-31 17:18:59 +0300

    added the search engine no Tests

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -24,12 +24,23 @@ class UserLogInApiView(ObtainAuthToken):
 
 class HotelViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.HotelSerializer
-    queryset = models.Hotel.objects.all()
+    # queryset = models.Hotel.objects.all()
+    def retrieve(self, request, pk=None):
+        choice = request.data['choice']
+        infoDict = dict(request.data['info'])
+        res = search_engine.get('HOTELS', choice, infoDict)
+        return Response({'result' : res})
+
 
 class PlaceViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.PlaceSerializer
 
-    queryset = models.Place.objects.all()
+        # queryset = models.Place.objects.all()
+    def retrieve(self, request, pk=None):
+        choice = request.data['choice']
+        infoDict = dict(request.data['info'])
+        res = search_engine.get('PLACES', choice, infoDict)
+        return Response({'result' : res})
 
 
 class UserProfileViewSet(viewsets.ModelViewSet):
