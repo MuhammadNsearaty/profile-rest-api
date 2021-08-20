8bcef147344b58ddee0e2e84e6e1d4b6265c9e70
MuhammadNsearaty <mnsyrat057@gmail.com>
2021-08-20 18:37:54 +0300

    update methods for the trips

--- planning_app/serializers.py
+++ planning_app/serializers.py
@@ -2,7 +2,7 @@
 import datetime
 
 from django.db.models import Count
-
+from django.db import transaction
 from rest_framework import serializers
 
 from planning_app import models
@@ -65,6 +65,20 @@ class PlaceDetailsSerializer(serializers.ModelSerializer):
                 'required': False,
             }
         }
+    def update(self,instance,validated_data):
+        instance.name = validated_data['name']
+        instance.description = validated_data['description']
+        instance.latitude = validated_data['latitude']
+        instance.longitude = validated_data['longitude']
+        instance.address = validated_data['address']
+        instance.distance = validated_data['distance']
+        instance.image = validated_data['image']
+        instance.city_name = validated_data['city_name']
+        instance.guest_rating = validated_data['guest_rating']
+        instance.properties = validated_data['properties']
+        instance.type = validated_data['type']
+        instance.save()
+        return instance
 
     def to_representation(self, instance):
         data = super().to_representation(instance)
@@ -97,6 +111,16 @@ class ActivityDetailsSerializer(serializers.ModelSerializer):
     def create(self,validated_data):
         activity = self.create(**validated_data)
         return activity
+    def update(self,instance,validated_data):
+        instance.day = validated_data['day']
+        instance.index = validated_data['index']
+        instance.place = validated_data['place']['id']
+        instance.save()
+        place_db = models.Place.objects.get(id=validated_data['place']['id'])
+        place_serilaizer = PlaceDetailsSerializer()
+        place_serilaizer.update(place_db,validated_data['place'])
+        return instance
+
 
 
 # TODO implement create method for nested representation
@@ -117,7 +141,18 @@ class DayDetailsSerializer(serializers.ModelSerializer):
         )
         serilaized_activities =  ActivityDetailsSerializer(data=activities_data,many=True)
         return day
-        
+    def update(self,instance,validated_data):
+        instance.day_index = validated_data['day_index']
+        instance.trip = validated_data['trip']
+        activties = validated_data.get('activities')
+        activities_serilaizer = ActivityDetailsSerializer() 
+        for activity in activties:
+            activity_db = models.Activity.objects.get(id=activity['id'])
+            if activity_db:
+                activities_serilaizer.update(activity_db,activity)
+            else:
+                models.Activity.objects.create(**activity)
+
     def to_representation(self, instance):
         data = super().to_representation(instance)
         for activity in data['activities']:
@@ -153,8 +188,22 @@ class TripDetailsSerializer(serializers.ModelSerializer):
             start_date = validated_data['start_date'],
         )
         serilaized_days = DayDetailsSerializer(data=days_data,read_only=True, many=True)
-        
         return trip
+    def update(self,instance,validated_data):
+        instance.id = validated_data['id']
+        instance.start_date = validated_data['start_date']
+        instance.save()
+
+        days = validated_data.get('days')
+        days_serilaizer = DayDetailsSerializer()
+        with transaction.atomic():
+            for day in days:
+                day_db = models.Day.get(id=day['id'])
+                if day_db:
+                    days_serilaizer.update(day_db,day)
+                else:
+                    models.Day.objects.create(**day)
+
 
     def validate_start_date(self, value):
         """
