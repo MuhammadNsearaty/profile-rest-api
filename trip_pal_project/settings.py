ef98a7943406a41099808f578711349da1bc31d8
Nitro963 <adnankattan9@gmail.com>
2021-08-28 18:13:51 +0300

    change database to mysql

--- trip_pal_project/settings.py
+++ trip_pal_project/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'trip_pal_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_trip_pal',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': ''
     }
 }
 
