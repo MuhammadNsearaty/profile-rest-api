a9207c539e056904a75d1073cc657f2e83d4e0a6
Nitro963 <adnankattan9@gmail.com>
2021-09-06 20:55:46 +0300

    rollback to sql lite engine

--- trip_pal_project/settings.py
+++ trip_pal_project/settings.py
@@ -82,10 +82,12 @@ WSGI_APPLICATION = 'trip_pal_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-with open('.env', 'r') as f:
-    DATABASES = {
-        'default': json.load(f)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
